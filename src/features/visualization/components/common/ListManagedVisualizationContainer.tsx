8a6a86b81c4482be4e402fce01fe1e596284b855
salmonco <xkqjsslsek80@naver.com>
2025-11-04 21:11:36 +0900

    feat: 불필요한 prop ㅅㅏㄱㅈㅔ

--- src/features/visualization/components/common/ListManagedVisualizationContainer.tsx
+++ src/features/visualization/components/common/ListManagedVisualizationContainer.tsx
@@ -1,45 +1,15 @@
-import React from "react";
 import { useMapList } from "~/maps/hooks/useMapList";
 
-interface Props {
-  // TODO: title 삭제
-  title?: string | React.ReactNode;
-  tooltip?: React.ReactNode;
-  filterContent?: React.ReactNode;
-}
-
-const ListManagedVisualizationContainer = ({ title, tooltip, filterContent }: Props) => {
+const ListManagedVisualizationContainer = () => {
   const mapList = useMapList();
 
   return (
     <div className="flex min-h-screen w-full flex-col gap-5 p-5">
       <div className="flex flex-col gap-5 rounded-lg bg-[#37445E] p-5">
-        {/* 타이틀 */}
-        <div className={`flex items-center gap-2.5 rounded-lg bg-[#43516D] px-5 text-white`}>
-          {typeof title === "string" ? (
-            <div className="flex items-center gap-2 py-4">
-              <p className="text-2xl font-semibold">{title}</p>
-              {tooltip}
-            </div>
-          ) : (
-            <div className="w-full py-3">{title}</div>
-          )}
-          {/* 툴팁 */}
-        </div>
-        <div className="flex h-[65%] min-h-[570px] gap-5 3xl:min-h-[800px] 4xl:min-h-[950px]">
-          {/* 지도 */}
-          <div style={{ flex: 7 }} className="rounded-lg">
-            {mapList.renderMaps()}
-          </div>
-          {/* 필터 */}
-          {filterContent && (
-            <div style={{ flex: 3 }} className="max-w-[360px] 3xl:max-w-[440px] 4xl:max-w-[550px]">
-              {filterContent}
-            </div>
-          )}
-        </div>
+        {/* 지도 */}
+        {mapList.renderMaps()}
         {/* 차트 */}
-        <div className="w-full">{mapList.renderFirstChart()}</div>
+        {mapList.renderFirstChart()}
       </div>
     </div>
   );
