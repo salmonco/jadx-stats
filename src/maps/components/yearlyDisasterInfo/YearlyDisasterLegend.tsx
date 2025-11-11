3452c4a00c7f77d9fd3f843f0bf7a03b0320504b
salmonco <xkqjsslsek80@naver.com>
2025-11-11 14:58:44 +0900

    feat: 농업재해 연도별 현황에 시각화설정 적용

--- src/maps/components/yearlyDisasterInfo/YearlyDisasterLegend.tsx
+++ src/maps/components/yearlyDisasterInfo/YearlyDisasterLegend.tsx
@@ -1,12 +1,18 @@
 import { useMemo } from "react";
-import YearlyDisasterInfoMap from "~/maps/classes/YearlyDisasterInfoMap";
-import { useMapList } from "~/maps/hooks/useMapList";
-import { colorsRed } from "~/utils/gisColors";
+import VisualizationLegend from "~/features/visualization/components/common/VisualizationLegend";
+import { YearlyDisasterFeatureCollection } from "~/features/visualization/layers/YearlyDisasterLayer";
+import { LegendColor, LegendOptions } from "~/maps/constants/visualizationSetting";
 
-const YearlyDisasterLegend = ({ mapId, features }) => {
-  const mapList = useMapList<YearlyDisasterInfoMap>();
-  const map = mapList.getMapById(mapId);
+interface Props {
+  features: YearlyDisasterFeatureCollection | null;
+  legendOptions: LegendOptions;
+  selectedDisasterCategory: string;
+  onLevelChange?: (level: number) => void;
+  onColorChange?: (color: LegendColor) => void;
+  onPivotPointsChange?: (pivotPoints: number[]) => void;
+}
 
+const YearlyDisasterLegend = ({ features, legendOptions, selectedDisasterCategory, onLevelChange, onColorChange, onPivotPointsChange }: Props) => {
   const { minValue, maxValue } = useMemo(() => {
     if (!features || !Array.isArray(features.features)) return { minValue: 0, maxValue: 0 };
 
@@ -14,7 +20,7 @@ const YearlyDisasterLegend = ({ mapId, features }) => {
     let max = -Infinity;
 
     for (const feature of features.features) {
-      const amt = feature?.properties?.stats?.[0]?.[map.selectedDisasterCategory];
+      const amt = feature?.properties?.stats?.[0]?.[selectedDisasterCategory];
       if (typeof amt === "number" && amt > 0) {
         min = Math.min(min, amt);
         max = Math.max(max, amt);
@@ -25,56 +31,19 @@ const YearlyDisasterLegend = ({ mapId, features }) => {
       minValue: min === Infinity ? 0 : min,
       maxValue: max === -Infinity ? 0 : max,
     };
-  }, [features]);
+  }, [features, selectedDisasterCategory]);
 
-  const gradientColors = [...colorsRed].reverse().join(", ");
-
+  // TODO: 범례에 단위 표기
+  // selectedDisasterCategory === "total_dstr_sprt_amt" ? "천원" : "m²"
   return (
-    <div className="flex flex-col gap-2 rounded-lg">
-      <div className="rounded-lg border border-[#d9d9d9] bg-white px-[8px] py-[8px] pb-[4px] shadow">
-        <div
-          className="h-[15px] rounded-md"
-          style={{
-            background: features?.features?.length === 1 ? colorsRed[6] : `linear-gradient(to right, ${gradientColors})`,
-          }}
-        />
-
-        <div className="mt-1 flex justify-between px-[2px] text-[14px] text-[#222]">
-          {features?.features?.length === 1 ? (
-            <span className="w-full text-center">
-              {minValue?.toLocaleString(undefined, { maximumFractionDigits: 0 })}
-              {map.selectedDisasterCategory === "total_dstr_sprt_amt" ? "천원" : "m²"}
-            </span>
-          ) : features?.features?.length === 2 ? (
-            <>
-              <span>
-                {minValue?.toLocaleString(undefined, { maximumFractionDigits: 0 })}
-                {map.selectedDisasterCategory === "total_dstr_sprt_amt" ? "천원" : "m²"}
-              </span>
-              <span>
-                {maxValue?.toLocaleString(undefined, { maximumFractionDigits: 0 })}
-                {map.selectedDisasterCategory === "total_dstr_sprt_amt" ? "천원" : "m²"}
-              </span>
-            </>
-          ) : (
-            <>
-              <span>
-                {minValue?.toLocaleString(undefined, { maximumFractionDigits: 0 })}
-                {map.selectedDisasterCategory === "total_dstr_sprt_amt" ? "천원" : "m²"}
-              </span>
-              <span>
-                {((minValue + maxValue) / 2)?.toLocaleString(undefined, { maximumFractionDigits: 0 })}
-                {map.selectedDisasterCategory === "total_dstr_sprt_amt" ? "천원" : "m²"}
-              </span>
-              <span>
-                {maxValue?.toLocaleString(undefined, { maximumFractionDigits: 0 })}
-                {map.selectedDisasterCategory === "total_dstr_sprt_amt" ? "천원" : "m²"}
-              </span>
-            </>
-          )}
-        </div>
-      </div>
-    </div>
+    <VisualizationLegend
+      minValue={minValue}
+      maxValue={maxValue}
+      legendOptions={legendOptions}
+      onLevelChange={onLevelChange}
+      onColorChange={onColorChange}
+      onPivotPointsChange={onPivotPointsChange}
+    />
   );
 };
 
