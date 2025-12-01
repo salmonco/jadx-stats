e2ed9afbd84af4d82e816a77bb3b30cc4000b062
salmonco <xkqjsslsek80@naver.com>
2025-12-02 02:39:58 +0900

    feat: 스펙트럼바 추가 (목데이터)

--- src/maps/components/yearlyDisasterInfo/YearlyDisasterInfoChart.tsx
+++ src/maps/components/yearlyDisasterInfo/YearlyDisasterInfoChart.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
-import { Table } from "lucide-react";
+import { BarChart3, Table } from "lucide-react";
+import DisasterSpectrumChart from "~/features/visualization/components/agriculturalEnvironment/DisasterSpectrumChart";
 import YearlyDisasterInfoTable from "~/features/visualization/components/agriculturalEnvironment/YearlyDisasterInfoTable";
 import { YearlyDisasterFeatureCollection } from "~/features/visualization/layers/YearlyDisasterLayer";
 import YearlyDisasterInfoMap from "~/maps/classes/YearlyDisasterInfoMap";
@@ -18,19 +19,31 @@ const YearlyDisasterInfoChart = ({ map, isReportMode }: Props) => {
 
   if (isReportMode) {
     return (
-      <div className="report-section w-full p-4">
-        <h3 className="mb-3 flex items-center gap-2 text-lg font-bold">
-          <Table size={24} />
-          <span>데이터 표</span>
-        </h3>
-        <YearlyDisasterInfoTable features={features} selectedDisaster={map.selectedDisaster} selectedDisasterCategory={map.selectedDisasterCategory} isReportMode />
-      </div>
+      <>
+        <div className="report-section w-full p-4">
+          <h3 className="mb-3 flex items-center gap-2 text-lg font-bold">
+            <Table size={24} />
+            <span>데이터 표</span>
+          </h3>
+          <YearlyDisasterInfoTable features={features} selectedDisaster={map.selectedDisaster} selectedDisasterCategory={map.selectedDisasterCategory} isReportMode />
+        </div>
+        <div className="w-full p-4">
+          <div className="report-section">
+            <h3 className="mb-3 flex items-center gap-2 text-lg font-bold">
+              <BarChart3 size={24} />
+              <span>데이터 그래프</span>
+            </h3>
+            <DisasterSpectrumChart features={features} selectedDisaster={map.selectedDisaster} selectedDisasterCategory={map.selectedDisasterCategory} isReportMode />
+          </div>
+        </div>
+      </>
     );
   }
 
   return (
     <div className="flex flex-col gap-4">
       <YearlyDisasterInfoTable features={features} selectedDisaster={map.selectedDisaster} selectedDisasterCategory={map.selectedDisasterCategory} />
+      <DisasterSpectrumChart features={features} selectedDisaster={map.selectedDisaster} selectedDisasterCategory={map.selectedDisasterCategory} />
     </div>
   );
 };
